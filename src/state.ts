9bc2d6980b62f474374eff173239d40b10ceeacb
Edward Tan <edward-tan.git@pm.me>
2025-09-11 12:30:06 +0800

    Add Tick,Flap,Bounce classes
    These classes implement the Action interface

--- src/state.ts
+++ src/state.ts
@@ -1,9 +1,10 @@
-import { State } from "./types";
+import { State, Action, Constants } from "./types";
 
 export const initialState: State = {
     lives: 3,
     score: 0,
     gameEnd: false,
+    y_velocity: 0,
 }; /**
  * Updates the state by proceeding with one time step.
  *
@@ -11,3 +12,32 @@ export const initialState: State = {
  * @returns Updated state
  */
 const tick = (s: State) => s;
+
+class Tick implements Action {
+    constructor(elapsed: number) {}
+
+    apply(s: State): State {
+        return {
+            ...s,
+            y_velocity: s.y_velocity + Constants.GRAVITY,
+        };
+    }
+}
+
+class Flap implements Action {
+    apply(s: State): State {
+        return {
+            ...s,
+            y_velocity: -4,
+        } as const;
+    }
+}
+
+class Bounce implements Action {
+    apply(s: State): State {
+        /** Placeholder */
+        return {
+            ...s,
+        } as const;
+    }
+}
