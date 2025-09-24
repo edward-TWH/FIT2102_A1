3e41a988dc10ca42380fd90172fbf0a33eeb0907
Edward Tan <edward-tan.git@pm.me>
2025-09-24 12:47:33 +0800

    Add attr function for setting attributes
    The attr is used to set multiple attributes of DOM objects
    Add placeholder parseCSV function

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,6 @@
-export { Vec };
+import { Observable } from "rxjs";
+
+export { Vec, parseCSV };
 
 /**
  * Class representing a 2D vector
@@ -12,3 +14,18 @@ class Vec {
     sub = (b: Vec) => this.add(b.scale(-1));
     scale = (s: number) => new Vec(this.x * s, this.y * s);
 }
+
+/**
+ * parses the contents from a csv request WIP
+ * @param contents
+ */
+function parseCSV(contents: String): Observable<Body> {}
+
+/**
+ * set a number of attributes on an Element at once
+ * @param e the Element
+ * @param o a property bag
+ */
+const attr = (e: Element, o: { [p: string]: unknown }) => {
+    for (const k in o) e.setAttribute(k, String(o[k]));
+};
